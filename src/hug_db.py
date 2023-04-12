a21463fbb9b1dea8a019b1dc7a92bc6d628a294f
alexndrvega <alexndr.vega+github@gmail.com>
2023-04-12 00:57:00 -0400

    Fully tested text

--- src/hug_db.py
+++ src/hug_db.py
@@ -25,13 +25,16 @@ class HugDB:
         item_vector = np.array(item_vector).astype('float32').reshape(1, -1)
         if self.index is None:
             self.index = faiss.IndexFlatL2(item_vector.shape[1])
+        faiss_id = self.index.ntotal
         self.index.add(item_vector)
         encrypted_vector = self.encrypt_vector(item_vector)
         self.id_to_data[item_id] = encrypted_vector
+        print(f"Added item with id: {item_id}")
     
     def search(self, query_vector, k=5):
         query_vector = np.array(query_vector).astype('float32').reshape(1, -1)
         distances, indices = self.index.search(query_vector, k)
-        encrypted_item_vectors = [self.id_to_data[i] for i in indices[0]]
+        print(f"indices: {indices}")
+        encrypted_item_vectors = [self.id_to_data[str(i)] for i in indices[0]]
         item_vectors = [self.decrypt_vector(encrypted_vector) for encrypted_vector in encrypted_item_vectors]
         return distances[0], item_vectors
