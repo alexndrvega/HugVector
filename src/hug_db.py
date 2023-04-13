bac951ff3dae66d6597c3f7dc516a3fff51d67ad
alexndrvega <alexndr.vega+github@gmail.com>
2023-04-12 23:22:08 -0400

    Include hybrid storage using faiss.IndexIVFPQ

--- src/hug_db.py
+++ src/hug_db.py
@@ -11,11 +11,14 @@ class HugItem:
         self.encrypted_vector = encrypted_vector
 
 class HugDB:
-    def __init__(self, encryption_key, nlist=100, nprobe=10, num_shards=1):
+    def __init__(self, encryption_key, nlist=100, nprobe=10, num_shards=1, M=16, nbits=8, index_filename=None):
         self.encryption = Fernet(encryption_key)
         self.nlist = nlist
         self.nprobe = nprobe
         self.num_shards = num_shards
+        self.M = M
+        self.nbits = nbits
+        self.index_filename = index_filename
         self.items = {}
         self.index = None
 
@@ -28,7 +31,7 @@ class HugDB:
     def build_index(self, data_vectors, metadata_list):
         data_vectors = np.array(data_vectors).astype('float32')
         quantizer = faiss.IndexFlatL2(data_vectors.shape[1])
-        index = faiss.IndexIVFFlat(quantizer, data_vectors.shape[1], self.nlist)
+        index = faiss.IndexIVFPQ(quantizer, data_vectors.shape[1], self.nlist, self.M, self.nbits)
         index.train(data_vectors)
 
         self.index = faiss.IndexShards(data_vectors.shape[1], self.num_shards)
@@ -36,13 +39,16 @@ class HugDB:
         for i, (vector, metadata) in enumerate(zip(data_vectors, metadata_list)):
             self.add_item(str(i), vector, metadata)
 
+        if self.index_filename is not None:
+            faiss.write_index(self.index, self.index_filename)
+
     def add_item(self, item_id, item_vector, metadata):
         item_vector = np.array(item_vector).astype('float32').reshape(1, -1)
         encrypted_vector = self.encrypt_vector(item_vector)
         item = HugItem(item_id, metadata, encrypted_vector)
         self.items[item_id] = item
         self.index.add(item_vector)
-        print(f"Added item with id: {item_id}")
+        print(f"Added item with id: {item_id}") #used for testing. (Remove at a later pull)
     
     def search(self, query_vector, k=5, metadata_filter=None):
         query_vector = np.array(query_vector).astype('float32').reshape(1, -1)
@@ -58,7 +64,7 @@ class HugDB:
                     filtered_distances.append(dist)
             indices, distances = np.array(filtered_indices).reshape(1, -1), np.array(filtered_distances).reshape(1, -1)
         
-        print(f"indices: {indices}")
+        print(f"indices: {indices}") #used for testing. (Remove at a later pull)
         encrypted_item_vectors = [self.items[str(i)].encrypted_vector for i in indices[0]]
         item_vectors = [self.decrypt_vector(encrypted_vector) for encrypted_vector in encrypted_item_vectors]
         return distances[0], item_vectors
